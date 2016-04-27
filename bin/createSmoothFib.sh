f9dc5e8a7c145570a56d217536ba7930ead85dec
Van-Tho Nguyen <thonv133@gmail.com>
2016-04-27 20:26:01 +0200

    shells

--- bin/createSmoothFib.sh
+++ bin/createSmoothFib.sh
@@ -5,21 +5,22 @@ outFib=$2
 iStep=$3
 
 dir="/home/vantho/ipol/demo/app/TDD_IPOLDemo/bin/"
+tempdir="/home/vantho/ipol/demo/app/TDD_IPOLDemo/tmp/"
 step=`echo "$iStep*0.2"|bc -l`
 echo $step
 
-scaledFile=${offFile%.off}-s0.2.off
-scaledFib=/tmp/scaledFib.xyz
+scaledFile=$tempdir${offFile%.off}-s0.2.off
+scaledFib=$tempdir/scaledFib.xyz
 
-/bin/sh - c "$dir"scaleOFF.sh 0.2 $offFile $scaledFile
+/bin/sh -c "$dir"scaleOFF.sh 0.2 $offFile $scaledFile
 "$dir"tubeAnalyse2 -i $scaledFile -r 40 --computeFiberPatchTrack /tmp/rawfile.xyz -a 0.3 \
 --exportOptiFiber $scaledFib -s "$step" \
 -n /tmp/accumulation.vol --fixRadiusInExportFiberMesh 1 --addSourceMeshExport $scaledFile #--invertNormal
 
 #restore
-/bin/sh - c "$dir"scaleXyz.sh 5 $scaledFib Fib.xyz
+/bin/sh -c "$dir"scaleXyz.sh 5 $scaledFib "$tempdir"Fib.xyz
 echo $offFile
-echo Splines -i $offFile -f Fib.xyz -o "$outFib"
-/bin/sh - c "$dir"Splines -i $offFile -f Fib.xyz -o "$outFib"
+echo Splines -i $offFile -f "$tempdir"Fib.xyz -o "$outFib"
+/bin/sh -c "$dir"Splines -i $offFile -f Fib.xyz -o "$outFib"
 
 exit 0
--- bin/onestep.sh
+++ bin/onestep.sh
@@ -6,12 +6,13 @@ bigStep=100
 smallStep=25
 #first time
 dir="/home/vantho/ipol/demo/app/TDD_IPOLDemo/bin/"
-/bin/sh -c "$dir"createSmoothFib.sh "$iFile" "$outFib" "$smallStep"
+tempdir="/home/vantho/ipol/demo/app/TDD_IPOLDemo/tmp/"
+/bin/sh -c "$dir"createSmoothFib.sh "$iFile" "$tempdir$outFib" "$smallStep"
 
 if [ -z "$output" ]; then
     output=oneStep
 fi
 echo thresholding -i "$iFile" -f "$outFib" -e 100 -a 25 -o "$output"
-/bin/sh -c "$dir"thresholding -i "$iFile" -f "$outFib" -e 100 -a 25 -o "$output"
+/bin/sh -c "$dir"thresholding -i "$iFile" -f "$tempdir$outFib" -e 100 -a 25 -o "$output"
 
 exit 0
