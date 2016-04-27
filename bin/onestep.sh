61d35efa57da435a495cd86426cfedcd86a91629
Van-Tho Nguyen <thonv133@gmail.com>
2016-04-27 23:06:16 +0200

    correction script

--- bin/onestep.sh
+++ bin/onestep.sh
@@ -6,12 +6,12 @@ bigStep=100
 smallStep=25
 #first time
 dir="/home/vantho/ipol/demo/app/TDD_IPOLDemo/bin/"
-/bin/sh "$dir"createSmoothFib.sh "$iFile" "$outFib" "$smallStep"
+/bin/sh -c '/home/vantho/ipol/demo/app/TDD_IPOLDemo/bin/createSmoothFib.sh ${0} ${1+"$@"}' "$iFile" "$outFib" "$smallStep"
 
 if [ -z "$output" ]; then
-    output=oneStep
+    output=rsDefects
 fi
 echo thresholding -i "$iFile" -f "$outFib" -e 100 -a 25 -o "$output"
-/bin/sh "$dir"thresholding -i "$iFile" -f "$outFib" -e 100 -a 25 -o "$output"
+"$dir"thresholding -i "$iFile" -f "$outFib" -e 100 -a 25 -o "$output"
 
 exit 0
