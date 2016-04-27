47dec286e6302da09b85f64fae6f5bb4eb822384
Van-Tho Nguyen <thonv133@gmail.com>
2016-04-27 21:44:58 +0200

    add /bin/sh

--- bin/createSmoothFib.sh
+++ bin/createSmoothFib.sh
@@ -11,13 +11,13 @@ echo $step
 scaledFile=${offFile%.off}-s0.2.off
 scaledFib=/tmp/scaledFib.xyz
 
-"$dir"scaleOFF.sh 0.2 $offFile $scaledFile
+/bin/sh "$dir"scaleOFF.sh 0.2 $offFile $scaledFile
 "$dir"tubeAnalyse2 -i $scaledFile -r 40 --computeFiberPatchTrack /tmp/rawfile.xyz -a 0.3 \
 --exportOptiFiber $scaledFib -s "$step" \
 -n /tmp/accumulation.vol --fixRadiusInExportFiberMesh 1 --addSourceMeshExport $scaledFile #--invertNormal
 
 #restore
-"$dir"scaleXyz.sh 5 $scaledFib Fib.xyz
+/bin/sh "$dir"scaleXyz.sh 5 $scaledFib Fib.xyz
 echo $offFile
 echo Splines -i $offFile -f Fib.xyz -o "$outFib"
-"$dir"Splines -i $offFile -f Fib.xyz -o "$outFib"
+/bin/sh "$dir"Splines -i $offFile -f Fib.xyz -o "$outFib"
