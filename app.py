fc0d2bf89ad5cfd827999d38f2b6f115a439e424
Van-Tho Nguyen <thonv133@gmail.com>
2016-04-27 20:07:04 +0200

    shell -e

--- app.py
+++ app.py
@@ -227,7 +227,7 @@ class app(base_app):
         
         f = open(self.work_dir+"output.txt", "w")
         fInfo = open(self.work_dir+"info.txt", "w")
-        command_args = ['/bin/sh', 'onestep.sh ' +  self.work_dir + "inputVol_0.off" + '  ' + self.work_dir + 'rsDefects' ]
+        command_args = ['/bin/sh', '-e', 'onestep.sh ' +  self.work_dir + "inputVol_0.off" + '  ' + self.work_dir + 'rsDefects' ]
 
         p = self.run_proc(command_args, stderr=fInfo, env={'LD_LIBRARY_PATH' : self.bin_dir})
         self.wait_proc(p, timeout=120)
