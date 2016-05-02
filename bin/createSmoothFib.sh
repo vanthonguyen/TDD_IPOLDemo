2a3535f6fa8829a5b0854b6ef36484fa8e61e36d
Nguyen Van Tho <thonv133@gmail.com>
2016-05-02 13:02:19 +0200

    remove hardcode

--- bin/createSmoothFib.sh
+++ bin/createSmoothFib.sh
@@ -5,20 +5,17 @@ outFib=$2
 iStep=$3
 echo $1
 
-dir="/home/vantho/ipol/demo/app/TDD_IPOLDemo/bin/"
 step=`echo "$iStep*0.2"|/usr/bin/bc -l`
-echo $step
 
 scaledFile=${offFile%.off}-s0.2.off
 scaledFib=/tmp/scaledFib.xyz
 
-/bin/bash "$dir"scaleOFF.sh 0.2 $offFile $scaledFile
-"$dir"tubeAnalyse2 -i $scaledFile -r 40 --computeFiberPatchTrack /tmp/rawfile.xyz -a 0.3 \
+scaleOFF.sh 0.2 $offFile $scaledFile
+tubeAnalyse2 -i $scaledFile -r 40 --computeFiberPatchTrack rawfile.xyz -a 0.3 \
 --exportOptiFiber $scaledFib -s "$step" \
--n /tmp/accumulation.vol --fixRadiusInExportFiberMesh 1 --addSourceMeshExport $scaledFile #--invertNormal
+-n accumulation.vol --fixRadiusInExportFiberMesh 1 --addSourceMeshExport $scaledFile #--invertNormal
 
 #restore
-/bin/sh "$dir"scaleXyz.sh 5 $scaledFib Fib.xyz
-echo $offFile
-echo Splines -i $offFile -f Fib.xyz -o "$outFib"
-"$dir"Splines -i $offFile -f Fib.xyz -o "$outFib"
+scaleXyz.sh 5 $scaledFib Fib.xyz
+
+Splines -i $offFile -f Fib.xyz -o "$outFib"
--- bin/onestep.sh
+++ bin/onestep.sh
@@ -5,13 +5,14 @@ outFib="${iFile%.off}-fib.xyz"
 bigStep=100
 smallStep=25
 #first time
-dir="/home/vantho/ipol/demo/app/TDD_IPOLDemo/bin/"
-/bin/sh -c '/home/vantho/ipol/demo/app/TDD_IPOLDemo/bin/createSmoothFib.sh ${0} ${1+"$@"}' "$iFile" "$outFib" "$smallStep"
+#/bin/sh -c '/home/vantho/ipol/demo/app/TDD_IPOLDemo/bin/createSmoothFib.sh ${0} ${1+"$@"}' "$iFile" "$outFib" "$smallStep"
+createSmoothFib.sh "$iFile" "$outFib" "$smallStep"
+
 
 if [ -z "$output" ]; then
     output=rsDefects
 fi
 echo thresholding -i "$iFile" -f "$outFib" -e 100 -a 25 -o "$output"
-"$dir"thresholding -i "$iFile" -f "$outFib" -e 100 -a 25 -o "$output"
+thresholding -i "$iFile" -f "$outFib" -e 100 -a 25 -o "$output"
 
 exit 0
